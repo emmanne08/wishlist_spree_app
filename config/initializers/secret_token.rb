6ac2c8b570305e9d4fe76e57dc52daf1c590d2d3
Emmanuel Cagadas <ec.secondary@gmail.com>
2013-07-20 00:17:25 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WishlistSpreeApp::Application.config.secret_token = '44143d3514581dbde56375dcab2cdad86f170792a9bfe8089ae3e8c13c23f97d858839104b2ac000957d59c67a35a1e06089578dffed36e0de1a77ed7e310a2c'
